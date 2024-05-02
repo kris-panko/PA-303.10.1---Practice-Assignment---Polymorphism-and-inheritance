d6b07201962885720e72a4261a96beb7fab6de85
kris-panko <krispanko@gmail.com>
2024-05-02 17:34:10 -0400

    first comm

---
+++ src/Monster.java
@@ -0,0 +1,10 @@
+public class Monster {
+    private String name;
+
+    public Monster(String name) {
+        this.name = name;
+    }
+    public String attack() {
+        return "!^_&^$@+%$* I don't know how to attack!";
+    }
+}
